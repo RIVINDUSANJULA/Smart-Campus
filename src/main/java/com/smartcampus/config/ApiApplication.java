a72723e928f1320fb4c8cadd5319344dcfb3354c
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-17 09:01:02 +0530

    ApiApplication setup - routing for .../api/v1

--- src/main/java/com/smartcampus/config/ApiApplication.java
+++ src/main/java/com/smartcampus/config/ApiApplication.java
@@ -1,4 +1,18 @@
 package com.smartcampus.config;
 
-public class ApiApplication {
+
+// JAX-RS --> Java API (REST Api - Web Services)
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/*
+    ApplicationPath - Base URI - Api
+    For Endpoints
+    API Versioning - v1
+    If Api Change - This Version will not break the app.
+*/
+
+@ApplicationPath("/api/v1")
+public class ApiApplication extends Application {
 }

abce93d382f002ef919bd1d4b18d4bfc4696305e
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-18 13:12:37 +0530

    Java for JSON - Get Data - Getters & Setters

---
+++ src/main/java/com/smartcampus/model/DiscoveryInfo.java
@@ -0,0 +1,18 @@
+package com.smartcampus.model;
+
+import java.util.Map;
+
+
+// Plain Old Java Object
+// Java --> JSON
+// JSON - Easy Read by Browser & Mobile
+
+public class DiscoveryInfo {
+    private String version;
+    private String adminContact;
+    private Map<String, String> endpoints;
+    // Above MAP - API route
+
+
+    public DiscoveryInfo() {}
+}

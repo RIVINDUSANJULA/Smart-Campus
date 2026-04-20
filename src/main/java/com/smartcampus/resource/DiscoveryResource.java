a90fee76ff84d066e11ee2ecee3f19b95ce1adad
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-20 18:45:16 +0530

    DiscoveryResource - API Setup

--- src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -1,4 +1,9 @@
 package com.smartcampus.resource;
 
+import javax.ws.rs.Path;
+
+// when someone visit PATH (/api/v1) --> This Answer
+// NEED TO CREATE ROOM CLASS
+@Path("")
 public class DiscoveryResource {
 }

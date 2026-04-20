c9162a7b1904ad3ebbe921ad3b7dd0ba9fed8c10
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-20 23:03:28 +0530

    HashMap Data --> GET --> List - (ROOMS)

--- src/main/java/com/smartcampus/resource/RoomResource.java
+++ src/main/java/com/smartcampus/resource/RoomResource.java
@@ -1,10 +1,17 @@
 package com.smartcampus.resource;
 
 
+import com.smartcampus.model.Room;
+import com.smartcampus.repository.DataStore;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/rooms")
 //Path - room
@@ -14,4 +21,13 @@ import javax.ws.rs.core.MediaType;
 // JSON POST
 @Consumes(MediaType.APPLICATION_JSON)
 public class RoomResource {
+
+    @GET
+    public Response getAllRooms() {
+        List<Room> allRooms = new ArrayList<>(DataStore.rooms.values());
+
+        return Response.ok(allRooms).build();
+        //200 --> Build
+    }
+    // Get all The Room - HashMap --> List
 }

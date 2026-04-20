fc5cd43330eda17aba8e79c376ec099d2ff4941d
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-20 19:16:25 +0530

    Get Req and COnvertor -JSON

--- src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -1,9 +1,24 @@
 package com.smartcampus.resource;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 // when someone visit PATH (/api/v1) --> This Answer
 // NEED TO CREATE ROOM CLASS
 @Path("")
 public class DiscoveryResource {
+
+    @GET
+    //Pnly Get Work. No one Can POST req to this. NO DATA SEND TO THIS URL
+
+    //Java Obj --> JSON
+    @Produces(MediaType.APPLICATION_JSON)
+
+
+    public Response getDiscoveryInfo(){
+
+    }
 }

0069f847ad27b498bce0ad8a045f75984e02ed87
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-20 21:17:18 +0530

    repo/DataStore - Var Ini

---
+++ src/main/java/com/smartcampus/repository/DataStore.java
@@ -0,0 +1,18 @@
+package com.smartcampus.repository;
+
+import com.smartcampus.model.Room;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.SensorReading;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DataStore {
+
+    public static final Map<String, Room> rooms = new ConcurrentHashMap<>();
+    public static final Map<String, Sensor> sensors = new ConcurrentHashMap<>();
+    public static final Map<String, List<SensorReading>> readings = new ConcurrentHashMap<>();
+
+
+}

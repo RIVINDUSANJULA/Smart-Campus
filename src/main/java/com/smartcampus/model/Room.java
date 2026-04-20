0db49d51346e13acd1176804fed6acaa264fbc70
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-20 19:44:38 +0530

    Room Ini & Var && Constructors Done

--- src/main/java/com/smartcampus/model/Room.java
+++ src/main/java/com/smartcampus/model/Room.java
@@ -8,17 +8,19 @@ public class Room {
     private String id;
     private String name;
     private int capacity;
-    private List<String> sensorIds;
+
+    private List<String> sensorId;
+    //Need Sensors Java
 
     public Room() {
-        this.sensorIds = new ArrayList<>();
+        this.sensorId = new ArrayList<>();
     }
 
     public Room(String id, String name, int capacity) {
         this.id = id;
         this.name = name;
         this.capacity = capacity;
-        this.sensorIds = new ArrayList<>();
+        this.sensorId = new ArrayList<>();
     }
 
 }

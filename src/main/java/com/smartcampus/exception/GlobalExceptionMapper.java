ab461195f1301e455a37e336313f762b91fe8cf3
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-21 21:17:40 +0530

    409

--- src/main/java/com/smartcampus/exception/GlobalExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/GlobalExceptionMapper.java
@@ -1,9 +1,23 @@
 package com.smartcampus.exception;
 
+import com.smartcampus.model.ErrorMessage;
+
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
 
+    @Override
+    public Response toResponse(Throwable ex) {
+        ErrorMessage errorInfo;
+        Response.Status status;
+
+        if (ex instanceof RoomNotEmptyException) {
+            errorInfo = new ErrorMessage(ex.getMessage(), 409, "https://api.smartcampus.com/docs/errors/409");
+            status = Response.Status.CONFLICT;
+        }
+        // Delete Attempt - Room BUT Sensor IS THERE --> Resource Conflict (409)
+    }
 }

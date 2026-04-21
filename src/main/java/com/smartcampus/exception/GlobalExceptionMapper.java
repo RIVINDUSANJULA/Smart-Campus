79bd9b828653e22d66cf015ce7ddc907f9b2e5d6
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-21 21:38:30 +0530

    500

--- src/main/java/com/smartcampus/exception/GlobalExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/GlobalExceptionMapper.java
@@ -22,7 +22,7 @@ public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
         // Delete Attempt - Room BUT Sensor IS THERE --> Resource Conflict (409)
 
 
-        if (ex instanceof LinkedResourceNotFoundException) {
+        else if (ex instanceof LinkedResourceNotFoundException) {
             errorInfo = new ErrorMessage(ex.getMessage(), 422, "https://api.smartcampus.com/docs/errors/422");
             return Response.status(422)
                     .entity(errorInfo)
@@ -34,10 +34,15 @@ public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
 
 
         // 403 - MAINTENANCE
-        if (ex instanceof SensorUnavailableException) {
+        else if (ex instanceof SensorUnavailableException) {
             errorInfo = new ErrorMessage(ex.getMessage(), 403, "https://api.smartcampus.com/docs/errors/403");
             status = Response.Status.FORBIDDEN;
         }
-        return null;
+
+        else {
+            errorInfo = new ErrorMessage("Internal Server Error: An unexpected failure occurred.", 500, "https://api.smartcampus.com/docs/errors/500");
+            status = Response.Status.INTERNAL_SERVER_ERROR;
+        }
+        // HTTP 500 Internal Server Error
     }
 }

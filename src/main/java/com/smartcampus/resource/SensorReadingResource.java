2737f806ceefa57246009d07ca5eb847fc6092e6
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-21 19:07:58 +0530

    Sensor Resources - Get - History of Sensors

--- src/main/java/com/smartcampus/resource/SensorReadingResource.java
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -1,8 +1,15 @@
 package com.smartcampus.resource;
 
+import com.smartcampus.model.SensorReading;
+import com.smartcampus.repository.DataStore;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -14,4 +21,13 @@ public class SensorReadingResource {
     public SensorReadingResource(String sensorId) {
         this.sensorId = sensorId;
     }
+
+    @GET
+    public Response getHistory() {
+        List<SensorReading> history = DataStore.readings.getOrDefault(sensorId, new ArrayList<>());
+        return Response.ok(history).build();
+    }
+    //Fetch Data
+    // All The Old Data of The Sensor that's alread in
+    // For New -> orDefault
 }

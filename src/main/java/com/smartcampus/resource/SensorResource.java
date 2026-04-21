ad306c0a7202ad48179f64f6cbb7c2f80b4bb1b0
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-21 20:41:07 +0530

    Sensor Data - GET with Filter

--- src/main/java/com/smartcampus/resource/SensorResource.java
+++ src/main/java/com/smartcampus/resource/SensorResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/sensors")
 @Produces(MediaType.APPLICATION_JSON)
@@ -21,6 +22,13 @@ public class SensorResource {
     @GET
     public Response getSensors(@QueryParam("type") String type) {
         List<Sensor> allSensors = new ArrayList<>(DataStore.sensors.values());
+
+        if (type != null && !type.trim().isEmpty()) {
+            List<Sensor> filtered = allSensors.stream()
+                    .filter(s -> type.equalsIgnoreCase(s.getType()))
+                    .collect(Collectors.toList());
+            return Response.ok(filtered).build();
+        }
 
         return Response.ok(allSensors).build();
     }

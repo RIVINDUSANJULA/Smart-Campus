c65691a30f307064a7f07a6c467b170538149bb0
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-21 20:40:04 +0530

    Sensor Data - GET & POST

--- src/main/java/com/smartcampus/resource/SensorResource.java
+++ src/main/java/com/smartcampus/resource/SensorResource.java
@@ -1,12 +1,24 @@
 package com.smartcampus.resource;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.repository.DataStore;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/sensors")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+// Path
+// JSON Get
+// JSON Post
+
 public class SensorResource {
+
+
+
+
 }

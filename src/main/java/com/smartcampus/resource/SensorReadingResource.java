a5756ec8746ea32b96d32a60734b0b10fa65587f
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-21 19:04:48 +0530

    Get & Post - Sensor Resources

---
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -0,0 +1,11 @@
+package com.smartcampus.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+// GET & POST
+public class SensorReadingResource {
+}
